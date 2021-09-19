a720cf04ce7ab8ba26b8bdf2619dc23f3e0f024c
TuzaoDPP <arthenrique231@gmail.com>
2021-09-19 14:07:22 -0300

    Scene Transition

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,19 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+  public Animator transition;
+  public float transitionTime;
+
+
   [Scene] public string scene;
   public bool nextScene;
 
   public void OnCollisionEnter2D(Collision2D collider)
   {
-    if (collider.gameObject.CompareTag("Player")) LoadScene();
+    if (collider.gameObject.CompareTag("Player")) LoadNextScene();
   }
 
-  public void LoadScene()
+  public void LoadNextScene()
   {
     if (!nextScene) SceneManager.LoadScene(scene);
-    else SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    else StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+  }
+
+  IEnumerator LoadScene(int levelIndex) 
+  {
+    transition.SetTrigger("Start");
+
+    yield return new WaitForSeconds (transitionTime);
+
+    SceneManager.LoadScene(levelIndex);
   }
 }
