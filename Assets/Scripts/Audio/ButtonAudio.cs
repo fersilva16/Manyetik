72b9d32358b54530a40f33fffe1c57b771ee62f8
Fernando Silva <fernando457829@gmail.com>
2021-10-06 23:34:30 -0300

    fix button audio script

--- Assets/Scripts/Audio/ButtonAudio.cs
+++ Assets/Scripts/Audio/ButtonAudio.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonSound : MonoBehaviour
+public class ButtonAudio : MonoBehaviour
 {
   [SerializeField]
   private AudioSource audioSource;
